13e5821dafcf66ec08eea2ba008328a52d2b7f80
aviel <aviel@aviel.com>
2018-07-08 14:50:28 +0300

    Add project files.

---
+++ ConsistTelerikTest/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ConsistTelerikTest._Default" %>
+
+<%@ Register assembly="Telerik.Web.UI" namespace="Telerik.Web.UI" tagprefix="telerik" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <telerik:RadGrid ID="RadGrid1" runat="server" OnNeedDataSource="RadGrid1_NeedDataSource">
+           
+        </telerik:RadGrid>
+    </div>
+
+</asp:Content>
---
+++ ConsistTelerikTest/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+
+namespace ConsistTelerikTest
+{
+
+
+
+    public partial class _Default : Page
+    {
+   
+        public _Default()
+        {
+ 
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadGrid1_NeedDataSource(object sender, GridNeedDataSourceEventArgs e)
+        {
+
+            RadGrid1.DataSource = MockDataStore.Instance.Data;
+        }
+
+    }
+    public class MockDataStore
+    {
+        private static MockDataStore _instance;
+        public static MockDataStore Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new MockDataStore());
+            }
+        }
+        public DataTable Data { get;  }
+    
+      
+        private MockDataStore()
+        {
+            Data = new DataTable();
+            Data.Columns.Add(new DataColumn() { ColumnName = "AccountId", Caption = "Account" });
+            Data.Columns.Add(new DataColumn() { ColumnName = "FirstName", Caption = "First Name" });
+            Data.Columns.Add(new DataColumn() { ColumnName = "LastName", Caption = "Last Name" });
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Data.Rows.Add(new object[] { i, string.Format("Name {0}", i), string.Format("LastName {0}", i) });
+            }
+         
+        }
+
+    }
+
+}
